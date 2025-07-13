af2577efa360986a5d807f5bcad30aab4101481e
Tee_blaq <olaifatimileyin2017@gmail.com>
2025-07-13 03:17:41 +0100

    Initializing addition

--- 9-add.js
+++ 9-add.js
@@ -1,5 +1,11 @@
 function add(a, b) {
   return a + b;
 }
-let result = add(3, 5);
-console.log(`The sum of 3 and 5 is: ${result}`);
+
+const args = process.argv.slice(2);
+const num1 = Number(args[0]);
+const num2 = Number(args[1]);
+
+console.log(add(num1, num2));
+// This code takes two command line arguments, converts them to numbers, and prints their sum.
+// If the arguments are not valid numbers, it will print NaN.
